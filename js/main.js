74693983b5d59565879028bcb55879e6dbb4d425
unknown <lujainshqer11@gmail.com>
2023-04-08 14:45:31 +0300

    withdraw details and inverstment modal and compunents the style

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ $(document).ready(function () {
    
     
     var scrollButton = $("#scroll-top"),
-        buyButton = $('#buy');
+        buyButton = $('#buy'),
+        amount = document.getElementsByClassName('amount-input');
+    // document.getElementById('show-money').innerHTML=document.getElementById('money').value;    
     //////Show The Scroll Top When I Down
 
     $(window).scroll(function () {
@@ -22,6 +24,14 @@ $(document).ready(function () {
         scrollButton.click(function () {
 
             $("html,body").animate({ scrollTop: 0 }, 2000);
+    
+        });
+        $('#home').click(function () {
+            $(this).addClass('active');
+            $(this).siblings().removeClass('active')
+            $("html , body").animate({
+                scrollTop: $('.home').offset().top 
+            }, 2000);
     
         });
         $('#about-us').click(function () {
@@ -48,6 +58,47 @@ $(document).ready(function () {
             }, 2000);
     
         });
+        ///////////Input File To Show An Image I Chosse It
+        const input = document.querySelector('input');
+        const preview = document.querySelector('.preview');
+    
+        input.addEventListener('change', updateImageDisplay);
+        function updateImageDisplay() {
+            while(preview.firstChild) {
+              preview.removeChild(preview.firstChild);
+            }
+            const curFiles = input.files;
+            if (curFiles.length === 0) {
+            } else {
+              const list = document.createElement('p');
+              preview.appendChild(list);
+          
+              for (const file of curFiles) {
+                if (validFileType(file)) {
+                  const image = document.createElement('img');
+                  image.src = URL.createObjectURL(file);
+          
+                  list.appendChild(image);
+                } 
+              }
+            }
+          }
+    const fileTypes = [
+        "image/apng",
+        "image/bmp",
+        "image/gif",
+        "image/jpeg",
+        "image/pjpeg",
+        "image/png",
+        "image/svg+xml",
+        "image/tiff",
+        "image/webp",
+        "image/x-icon"
+      ];
+      
+      function validFileType(file) {
+        return fileTypes.includes(file.type);
+      }
 });
 
 // $(window).onload(function () {
