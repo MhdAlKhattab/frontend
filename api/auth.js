218599480c9ac31605ea7127a4c06dd3222d4297
Mhd Al Khattab <mohammadalkhatab123@gmail.com>
2023-05-10 22:02:02 +0300

    Forget My Password API

--- api/auth.js
+++ api/auth.js
@@ -46,22 +46,86 @@ $(document).ready(function () {
         });
     });
 
-    // Change Passport
-    $("#changePassportForm").submit(function (e) {
+    // Forget My Password
+    $("#forgetForm").submit(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "http://127.0.0.1:8000/api/forget-password",
+            type: "POST",
+            data: $("#forgetForm").serialize(),
+            dataType: 'JSON',
+            success: function (data) {
+                console.log("yeees");
+                $(location).attr('href', 'reset-password.html');
+            },
+            error: function (error) {
+                $('.error-message').empty().append('<span class="badge badge-danger">User Does not Exists</span>');
+            }
+        });
+    });
+
+    // Reset Password
+    $("#resetPasswordForm").submit(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "http://127.0.0.1:8000/api/reset-password",
+            type: "POST",
+            data: $("#resetPasswordForm").serialize(),
+            dataType: 'JSON',
+            success: function (data) {
+                console.log("yeees");
+                $(location).attr('href', 'login.html');
+            },
+            error: function (error) {
+                output = '';
+                if (error.responseJSON.errors) {
+                    for (var er in error.responseJSON.errors) {
+                        output += `
+                            ${error.responseJSON.errors[er][0]}
+                            <br>
+                        `;
+                    }
+                } else {
+                    output = error.responseJSON.data;
+                }
+
+                $('.error-message').empty().append('<span class="badge badge-danger">' + output + '</span>');
+            }
+        });
+    });
+
+    // Change Password
+    $("#changePasswordForm").submit(function (e) {
         e.preventDefault();
 
         $.ajax({
             url: "http://127.0.0.1:8000/api/change-password",
             headers: { "Authorization": "Bearer " + localStorage.getItem('access_token') },
             type: "POST",
-            data: $("#changePassportForm").serialize(),
+            data: $("#changePasswordForm").serialize(),
             dataType: "json",
             success: function (data) {
                 localStorage.setItem('access_token', data.access_token);
                 $(location).attr('href', 'control.html');
             },
             error: function (error) {
-                $('.error-message').empty().append('<span class="badge badge-danger">Invalid login details</span><br>');
+                output = '';
+                if (error.responseJSON.errors) {
+                    for (var er in error.responseJSON.errors) {
+                        output += `
+                            ${error.responseJSON.errors[er][0]}
+                            <br>
+                        `;
+                    }
+                } else {
+                    output = error.responseJSON.data;
+                }
+
+                $('.error-message').empty().append('<span class="badge badge-danger">' + output + '</span>');
+
+                // $('.error-message').empty().append('<span class="badge badge-danger">Invalid login details</span><br>');
             }
         });
     });
