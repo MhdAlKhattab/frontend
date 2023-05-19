df099cb5879a8b189e46a5258d1ada1c02ea8407
Mhd Al Khattab <mohammadalkhatab123@gmail.com>
2023-05-19 22:24:03 +0300

    Fixed Referral Register

--- api/auth.js
+++ api/auth.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
 
+    var id = window.location.search.split('=')[1];
+    if (id){
+        console.log("Ok", id);
+        document.getElementById("referralInput").value = id;
+    }
+
     // Sign Up
     $("#signForm").submit(function (e) {
         e.preventDefault();
