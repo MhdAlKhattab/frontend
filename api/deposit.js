4da04fb97619ee199cb8b0c7f171da74188b4daf
unknown <lujainshqer11@gmail.com>
2023-05-25 20:36:56 +0300

    other api

--- api/deposit.js
+++ api/deposit.js
@@ -63,4 +63,40 @@ $(document).ready(function () {
         });
     });
     // End Add Deposit
+     // Start Add other Deposit
+     $('body').on('submit', '#depositForm', function(e){
+        e.preventDefault();
+
+        var data = new FormData(this);
+
+        $.ajax({
+            url: "http://127.0.0.1:8000/api/add-deposit",
+            type: "POST",
+            headers: { "Authorization": "Bearer " + localStorage.getItem('access_token') },
+            data: data,
+            dataType: "json",
+            processData: false,
+            contentType: false,
+            success: function (data) {
+                $(location).attr('href', 'control.html');
+
+            },
+            error: function (error) {
+                output = '';
+                if (error.responseJSON.errors) {
+                    for (var er in error.responseJSON.errors) {
+                        output += `
+                            ${error.responseJSON.errors[er][0]}
+                            <br>
+                        `;
+                    }
+                }else{
+                    output = error.responseJSON.data;
+                }
+
+                $('.error-message').empty().append('<span class="badge badge-danger">' + output + '</span>');
+            }
+        });
+    });
+    // End Add Deposit
 });
